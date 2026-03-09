e3cbb11ebb58986b66009c1c1f970bf28746e52a
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-03-09 17:52:46 +0530

    ai is great, taking our jobs, worked with just one prompt, now i have a chess board

--- learning/tilemap_from_scrach/main.js
+++ learning/tilemap_from_scrach/main.js
@@ -10,15 +10,13 @@ const winWidth = window.innerWidth
 const tileHeight = 100
 const tileWidth = 100
 
-ctx.fillStyle = "blue"
 for (let i = 0; i <= winWidth/tileWidth; i++) {
     for (let j = 0; j <= winHeight/tileHeight; j++) {
-        ctx.fillRect(tileWidth*i, tileHeight*j, tileWidth, tileHeight)
-        console.log(ctx.fillStyle)
-        if (ctx.fillStyle == "#0000ff") {
-            ctx.fillStyle = "white"
-        } else {
+        if ((i + j) % 2 === 0) {
             ctx.fillStyle = "blue"
+        } else {
+            ctx.fillStyle = "white"
         }
+        ctx.fillRect(tileWidth*i, tileHeight*j, tileWidth, tileHeight)
     }
 }
